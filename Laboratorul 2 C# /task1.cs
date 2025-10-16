a762a90a9d9712c37605e0e43b18bfbe324fbd93
Andy Powered <andreigultur@gmail.com>
2025-10-16 17:06:59 +0300

    Update task1.cs

--- Laboratorul 2 C# /task1.cs
+++ Laboratorul 2 C# /task1.cs
@@ -1,1 +1,43 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introdu numarul natural:");
+        string numar = Console.ReadLine();
+
+        char cifraMax = numar[0];
+        int maxLungime = 1;
+
+        char cifraCurenta = numar[0];
+        int lungimeCurenta = 1;
+
+        for (int i = 1; i < numar.Length; i++)
+        {
+            if (numar[i] == cifraCurenta)
+            {
+                lungimeCurenta++;
+            }
+            else
+            {
+                if (lungimeCurenta > maxLungime)
+                {
+                    maxLungime = lungimeCurenta;
+                    cifraMax = cifraCurenta;
+                }
+                cifraCurenta = numar[i];
+                lungimeCurenta = 1;
+            }
+        }
+
+        if (lungimeCurenta > maxLungime)
+        {
+            maxLungime = lungimeCurenta;
+            cifraMax = cifraCurenta;
+        }
+
+        Console.WriteLine($"Cifra care apare consecutiv de cele mai multe ori: {cifraMax}");
+        Console.WriteLine($"Lungimea secventei: {maxLungime}");
+    }
+}
